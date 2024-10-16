17658ade30780df951faef1cf10c93027b4c9de7
aibaljose <aibaljosej@gmail.com>
2024-10-16 22:58:22 +0530

    Added landing page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="nav">
+        <div class="logo">BEHAVE</div>
+        <div class="menu">
+            <ul>
+                <li>Home</li>
+                <li>Events</li>
+                <li>Profile</li>
+                <div class="login">Login</div>
+                <div class="signup">Sign in</div>
+            </ul>
+        </div>
+    </div>
+    <div class="hero">
+       <div class="maincard">
+
+       </div>
+       <div class="sidecard">
+        
+       </div>
+    </div>
+    
+</body>
+</html>
